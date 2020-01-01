a5c02de3e63b5e646f409f812076b3399c50899a
hppRC <hpp.ricecake@gmail.com>
2020-01-01 19:49:48 +0900

    succedss

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+
+// This is using the `tokio` runtime. You'll need the following dependency:
+//
+// `tokio = { version = "0.2", features = ["macros"] }`
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://hyper.rs").await?;
+
+    println!("Status: {}", res.status());
+
+    let body = res.text().await?;
+
+    println!("Body:\n\n{}", body);
+
+    Ok(())
 }
