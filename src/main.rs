5933c4ac9805c8221888b845a8e930fd91a8a33a
hppRC <hpp.ricecake@gmail.com>
2020-01-01 22:31:51 +0900

    success scraping

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
-#![deny(warnings)]
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
-// This is using the `tokio` runtime. You'll need the following dependency:
-//
-// `tokio = { version = "0.2", features = ["macros"] }`
+extern crate regex;
+
+use std::collections::HashSet;
+
+use regex::Regex;
+
+use scraper::{Html, Selector};
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let res = reqwest::get("https://hyper.rs").await?;
+    let mut stack: Vec<String> = vec![];
+
+    let res = reqwest::get("http://aoj-icpc.ichyo.jp/").await?;
 
     println!("Status: {}", res.status());
 
-    let body = res.text().await?;
+    let html = res.text().await?;
+    let document = Html::parse_document(&html);
+    let css = "tr td a";
+    let selector = Selector::parse(css).unwrap();
 
-    println!("Body:\n\n{}", body);
+    let re = Regex::new(r"id=(\d{4})").unwrap();
+
+    for node in document.select(&selector) {
+        if node.value().attr("target").unwrap_or("") == "_blank" {
+            let test = node.value().attr("href").unwrap_or("").to_string();
+            let caps = re.captures(&test);
+            match caps {
+                Some(ret) => {
+                    stack.push((&ret[1]).to_string());
+                }
+                None => {
+                    println!("{:?}", caps);
+                }
+            }
+        }
+    }
+
+    println!("{:?}", stack);
+    let uniq = stack
+        .into_iter()
+        .collect::<HashSet<String>>()
+        .into_iter()
+        .collect::<Vec<String>>();
+    println!("{:?}", uniq);
 
     Ok(())
 }
